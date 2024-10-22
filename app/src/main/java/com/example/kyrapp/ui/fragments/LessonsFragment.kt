6f491dd2e447cb5e010226ceef1251a7317a9ae7
Nursultan Kasymbekov <nkasymbekov99@gmail.com>
2024-10-22 18:13:19 +0600

    replace bookmark icons

--- app/src/main/java/com/example/kyrapp/ui/fragments/LessonsFragment.kt
+++ app/src/main/java/com/example/kyrapp/ui/fragments/LessonsFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kyrapp.R
 import com.example.kyrapp.adapters.ItemAdapter
+import com.example.kyrapp.databinding.LessonItemBinding
 import com.example.kyrapp.viewModel.ItemViewModel
 
 
